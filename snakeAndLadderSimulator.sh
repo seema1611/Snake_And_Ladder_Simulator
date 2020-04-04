826404c27daa2bd1aee11f962154fdbf407d1e5d
seema1611 <seemarajpure16@gmail.com>
2020-04-04 19:37:10 +0530

    ADDED: Checked for the NO_PLAY, LADDER and SNAKE

--- snakeAndLadderSimulator.sh
+++ snakeAndLadderSimulator.sh
@@ -2,11 +2,44 @@
 
 #Constants
 START_POSITION=0
+NO_PLAY=0
+LADDER=1
+SNAKE=2
+
+
+#Variable
+position_Of_Player=0
 
 #Function to generate random between 1-6
 function rollTheDie() {
 
-	echo "Random: "$((RANDOM%6 +1))
+	addNumber=$((RANDOM%6 +1))
 }
 
-rollTheDie
+#Function to generate random for cases
+function getOption() {
+
+	option=$((RANDOM%3))
+}
+
+#Function to check for NO_PLAY, SNAKE and LADDER
+function main() {
+
+	rollTheDie
+	getOption
+	case $option in
+		$NO_PLAY)
+			position_Of_Player=$position_Of_Player
+			;;
+		$LADDER)
+			position_Of_Player=$(($position_Of_Player+$addNumber))
+			;;
+		$SNAKE)
+			position_Of_Player=$(($position_Of_Player-$addNumber))
+			;;
+	esac
+
+}
+
+#Main function call
+main
